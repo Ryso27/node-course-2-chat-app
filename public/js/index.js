0b08014fb28ab0de3774af3b3b419bd22e4f8a36
Ryan <sobbe.ryan@gmail.com>
2019-01-17 21:40:02 -0600

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,27 +2,12 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server.');
-
-  socket.emit('createEmail', {
-    to: 'jen@email.com',
-    text: 'hey, this is jen from email'
-  });
-
-  socket.emit('createMessage', {
-    to: 'jen@message.com',
-    text: 'hey, this is jen from chat'
-  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server.');
 });
 
-//listener for newEmail event
-socket.on('newEmail', function(email) {
-  console.log('New email', email);
-});
-
-socket.on('newMessage', function(message) {
-  console.log('New message', message);
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
 });
--- server/server.js
+++ server/server.js
@@ -15,31 +15,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
   console.log('New user connected.');
 
-  //emit event, passing object
-  socket.emit('newEmail', {
-    from: 'mike@example.com',
-    text: 'Hey, what is going on.',
-    createAt: 123
-  });
-
-  socket.emit('newMessage', {
-    from: 'ryan@wearehone.com',
-    text: 'Hey whats up.',
-    createdAt: Date.now()
-  });
-
-  socket.on('createEmail', (newEmail) =>{
-    console.log('createEmail', newEmail);
-  });
-
-  socket.on('createMessage', (newMessage)=>{
-    console.log('createMessage', newMessage);
+  socket.on('createMessage', (message)=>{
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', ()=>{
     console.log('User was disconnected.');
   });
-  
+
 });
 
 server.listen(port, ()=>{
