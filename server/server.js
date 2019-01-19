8017952a6b3579eab690d6c33324c371b90cfbdf
Ryan <sobbe.ryan@gmail.com>
2019-01-19 16:09:09 -0600

    cleaning up code

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ const http = require('http')
 const express = require('express');
 const socketIO = require('socket.io');
 
-
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
-
 
 app.use(express.static(publicPath));
 
